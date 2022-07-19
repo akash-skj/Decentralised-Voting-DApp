d9ee4aef512c090fe14df163a62cb82ef23d7646
xkxshs <akashsuresh.sj@gmail.com>
2022-07-20 00:14:51 +0530

    Implemented React Router!

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -1,17 +1,15 @@
 const Result= () => {
 
-
-
     return(
         <div>
-        <div className="flex flex-col justify-center items-center h-full m-10 mb-5 text-2xl bg-honey-brown text-honey-gold font-semibold rounded-xl">
-            <div className="flex py-[70px] flex-wrap justify-content items-center">
-                
-                <H1>Result:!!</H1>
+            <div className="flex flex-col justify-center items-center h-full m-10 mb-5 text-2xl bg-honey-brown text-honey-gold font-semibold rounded-xl">
+                <div className="flex py-[70px] flex-wrap justify-content items-center">
+                    
+                    <h1>Result:!!</h1>
 
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
--- client/src/components/Vote.jsx
+++ client/src/components/Vote.jsx
@@ -9,7 +9,6 @@ const Vote = () => {
             <div className="flex flex-col justify-center items-center h-full m-10 mb-5 text-2xl bg-honey-brown text-honey-gold font-semibold rounded-xl">
                 <div className="flex py-[70px] flex-wrap justify-content items-center">
                     {candidates.map((candidate)=>(<Candidate candidate={candidate}/>))}
-
                 </div>
             </div>
         </div>
